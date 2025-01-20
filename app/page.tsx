b7c9b3c5d79c16039defb8964b023db3989ad3c0
ewhalenkamp <70407222+ewhalenkamp@users.noreply.github.com>
2025-01-20 14:39:19 -0700

    header hides on scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+"use client"
 import Header from "@/components/header";
 
 export default function Page() {
-    return <><Header /></>
+    return <>
+        <Header />
+        <section id="about" style={{ width: "100%", height: "1000px" }}></section>
+        <section id="projects" style={{ width: "100%", height: "1000px", backgroundColor: "#8249cc" }}></section>
+    </>
 }
